05eb2c8a728ead9246e3599a7923aa57827f8921
Edward 'Ted <159036536+Swillycoder@users.noreply.github.com>
2025-07-16 19:58:20 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Replace this with your backend URL deployed on Render
-const BACKEND_URL = "https://web3backend-ahbi.onrender.com/verify";
+const BACKEND_URL = "https://web3backend-ahbi.onrender.com";
 
 async function connectWallet() {
   if (!window.ethereum) {
